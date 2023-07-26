43514f8cbd3b98b2df1528a138f412e68aac6588
Christopher M <mrcm8263@gmail.com>
2023-07-26 18:06:01 +1000

    Start programmatically adding data to SaveWindow

--- ExocolonistSaveEditor/Windows/SaveWindow.xaml.cs
+++ ExocolonistSaveEditor/Windows/SaveWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ExocolonistSaveEditor.Core.Models;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace ExocolonistSaveEditor.Windows
@@ -8,11 +10,29 @@ namespace ExocolonistSaveEditor.Windows
     /// </summary>
     public partial class SaveWindow : Window
     {
+        [GeneratedRegex("(?<!^)(?=[A-Z])")]
+        private static partial Regex Uppercase();
+
         public SaveWindow(Save save)
         {
             InitializeComponent();
 
-            CharacterName.Text = save.PrincessName;
+            Title = $"{save.FileName} - Exocolonoist Saved Editor";
+
+            foreach (PropertyInfo field in typeof(Save).GetProperties())
+            {
+                object value = field.GetValue(save) ?? "";
+
+                if (value is null || string.IsNullOrEmpty(value.ToString()))
+                {
+                    continue;
+                }
+
+                if (value is string)
+                {
+                    SaveData.Items.Add(new { Key = string.Join(" ", Uppercase().Split(field.Name)), Value = field.GetValue(save) });
+                }
+            }
         }
     }
 }
